7c7f0342bab5b97752d7fc95164ac9d5290a8738
Ricky <twonine293@gmail.com>
2025-04-26 18:10:54 -0300

    atualização 2.0

--- script.js
+++ script.js
@@ -7,3 +7,22 @@
     menuLateral.classList.toggle('active');
   });
 
+
+  const links = document.querySelectorAll('.toggle');
+
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      const descricao = this.nextElementSibling;
+  
+      if (descricao.style.display === 'none' || descricao.style.display === '') {
+        descricao.style.display = 'block';
+        this.classList.add('ativo'); // adiciona negrito
+      } else {
+        descricao.style.display = 'none';
+        this.classList.remove('ativo'); // tira o negrito
+      }
+    });
+  });
+  
